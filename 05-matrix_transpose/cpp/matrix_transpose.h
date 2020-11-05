a4a31a8d2139f5cd11ef8c64b72e2db9a24e0548
jiriklepl <jiriklepl@seznam.cz>
2020-11-06 00:59:42 +0100

    implemented

--- 05-matrix_transpose/cpp/matrix_transpose.h
+++ 05-matrix_transpose/cpp/matrix_transpose.h
@@ -16,9 +16,33 @@
 
 void transpose()
 {
-    // TODO: Implement this efficiently
+    trans(0,0, N);
+}
 
-    for (unsigned i=0; i<N; i++)
-        for (unsigned j=0; j<i; j++)
-            swap(i, j, j, i);
+void trans(unsigned i, unsigned j, unsigned width) {
+    if (width > 1) {
+        const unsigned n_width = width - width / 2;
+        trans(i + n_width, j + n_width, width - n_width);
+        cis(i + n_width, j, width - n_width, n_width, i, j + n_width);
+        return trans(i, j, n_width);
+    }
+}
+
+void cis(unsigned i1, unsigned j1, unsigned width, unsigned height , unsigned i2, unsigned j2) {
+    if (width < height) {
+        cis(i1, j1 + width, width, height - width, i2 + width, j2);
+    } else if (width > height) {
+        cis(i1 + height, j1, width - height, height, i2, j2 + height);
+        width = height;
+    }
+
+    trans(i2, j2, width);
+    swap_sq(i1, j1, width, i2, j2);
+    return trans(i2, j2, width);
+}
+
+void swap_sq(unsigned i1, unsigned j1, unsigned width, unsigned i2, unsigned j2) {
+        for (unsigned i = 0; i < width; ++i)
+            for (unsigned j = 0; j < width; ++j)
+                swap(i1 + i, j1 + j, i2 + i, j2 + j);
 }
