5188e11e632eef3ea42f01e56ca87870821a4d56
jiriklepl <jiriklepl@seznam.cz>
2020-11-06 01:09:51 +0100

    style

--- 05-matrix_transpose/cpp/matrix_transpose.h
+++ 05-matrix_transpose/cpp/matrix_transpose.h
@@ -14,18 +14,16 @@
  *      }
  */
 
-void transpose()
-{
-    trans(0,0, N);
-}
+void transpose() { trans(0,0, N); }
 
 void trans(unsigned i, unsigned j, unsigned width) {
-    if (width > 1) {
-        const unsigned n_width = width - width / 2;
-        trans(i + n_width, j + n_width, width - n_width);
-        cis(i + n_width, j, width - n_width, n_width, i, j + n_width);
-        return trans(i, j, n_width);
-    }
+    if (width == 1)
+        return;
+    
+    const unsigned n_width = width - width / 2;
+    trans(i + n_width, j + n_width, width - n_width);
+    cis(i + n_width, j, width - n_width, n_width, i, j + n_width);
+    trans(i, j, n_width);
 }
 
 void cis(unsigned i1, unsigned j1, unsigned width, unsigned height , unsigned i2, unsigned j2) {
@@ -38,11 +36,11 @@ void cis(unsigned i1, unsigned j1, unsigned width, unsigned height , unsigned i2
 
     trans(i2, j2, width);
     swap_sq(i1, j1, width, i2, j2);
-    return trans(i2, j2, width);
+    trans(i2, j2, width);
 }
 
 void swap_sq(unsigned i1, unsigned j1, unsigned width, unsigned i2, unsigned j2) {
-        for (unsigned i = 0; i < width; ++i)
-            for (unsigned j = 0; j < width; ++j)
-                swap(i1 + i, j1 + j, i2 + i, j2 + j);
+    for (unsigned i = 0; i < width; ++i)
+        for (unsigned j = 0; j < width; ++j)
+            swap(i1 + i, j1 + j, i2 + i, j2 + j);
 }
