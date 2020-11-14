b32358e14b9e01846bb09cb04637b40d5a72cf26
jiriklepl <jiriklepl@seznam.cz>
2020-11-14 02:57:57 +0100

    the lecture algorithm

--- 05-matrix_transpose/cpp/matrix_transpose.h
+++ 05-matrix_transpose/cpp/matrix_transpose.h
@@ -14,137 +14,35 @@
  *      }
  */
 
-void transpose() { trans(0U, 0U, N); }
-
-std::tuple<unsigned, unsigned char> max_pow2(unsigned fits) {
-    unsigned power = 1U;
-    unsigned char log = 0U;
-
-    while ((fits /= 2U) >= 1U) {
-        power *= 2U;
-        ++log;
-    }
-
-    return {power, log};
-}
+void transpose() { trans(0, 0, N); }
 
 void trans(unsigned i, unsigned j, unsigned width) {
-    if (width > 1U) {
-        auto &&[power, log] = max_pow2(width);
-        const unsigned n_width = power / 2U;
-        if (power != width) {
-            trans(i + n_width, j + n_width, width - n_width);
-            pre_cis(i + n_width, j, width - n_width, n_width, i, j + n_width);
-            pre_trans_static(i, j, log - 1U);
-        } else {
-            pre_trans_static(i, j, log);
+    if (width > 1) {
+        const unsigned r_width = width / 2; // also b_height
+        const unsigned l_width = width - r_width; // also t_height
+
+        trans(i, j, l_width); // top left
+        trans_swap(i + l_width, j, r_width, l_width); // bottom left
+        trans(i + l_width, j + l_width, r_width); // bottom right
+    }
+}
+
+void trans_swap(unsigned i, unsigned j, unsigned height, unsigned width) {
+    if (height == 1 || width == 1) {
+        if (i != j) {
+            swap(i, j, j, i);
+            if (height == 2 || width == 2)
+                swap(i + height - 1, j + width - 1, j + width - 1, i + height - 1);
         }
-    }
-}
+    } else {
+        const unsigned b_height = height / 2;
+        const unsigned t_height = height - b_height;
+        const unsigned r_width = width / 2;
+        const unsigned l_width = width - r_width;
 
-void pre_cis(unsigned i1, unsigned j1, unsigned width, unsigned height, unsigned i2, unsigned j2) {
-    switch ((width <= height) + (width == height)) {
-    case 0U: /* width > height */
-        cis(i1, j1, width, height, i2, j2);
-        break;
-    case 1U: /* width < height */
-        cis(i2, j2, height, width, i1, j1);
-        break;
-    default: /* width == height */
-        trans(i1, j1, width);
-        swap_sq(i1, j1, width, i2, j2);
-        trans(i1, j1, width);
-        break;
-    }
-}
-
-void cis(unsigned i1, unsigned j1, unsigned width, unsigned height, unsigned i2, unsigned j2) {
-    const unsigned n_width = width - width / 2U;
-    pre_cis(i2, j2, height, n_width, i1, j1);
-    pre_cis(i2, j2 + n_width, height, width - n_width, i1 + n_width, j1);
-}
-
-void swap_sq(unsigned i1, unsigned j1, unsigned width, unsigned i2, unsigned j2) {
-    for (unsigned i = 0U; i < width; ++i)
-        for (unsigned j = 0U; j < width; ++j)
-            swap(i1 + i, j1 + j, i2 + i, j2 + j);
-}
-
-void pre_trans_static(unsigned i, unsigned j, unsigned char log) {
-    switch (log) {
-    case 19U:
-        trans_static<1U << 19U>(i, j);
-        break;
-    case 18U:
-        trans_static<1U << 18U>(i, j);
-        break;
-    case 17U:
-        trans_static<1U << 17U>(i, j);
-        break;
-    case 16U:
-        trans_static<1U << 16U>(i, j);
-        break;
-    case 15U:
-        trans_static<1U << 15U>(i, j);
-        break;
-    case 14U:
-        trans_static<1U << 14U>(i, j);
-        break;
-    case 13U:
-        trans_static<1U << 13U>(i, j);
-        break;
-    case 12U:
-        trans_static<1U << 12U>(i, j);
-        break;
-    case 11U:
-        trans_static<1U << 11U>(i, j);
-        break;
-    case 10U:
-        trans_static<1U << 10U>(i, j);
-        break;
-    case 9U:
-        trans_static<1U << 9U>(i, j);
-        break;
-    case 8U:
-        trans_static<1U << 8U>(i, j);
-        break;
-    case 7U:
-        trans_static<1U << 7U>(i, j);
-        break;
-    case 6U:
-        trans_static<1U << 6U>(i, j);
-        break;
-    case 5U:
-        trans_static<1U << 5U>(i, j);
-        break;
-    case 4U:
-        trans_static<1U << 4U>(i, j);
-        break;
-    case 3U:
-        trans_static<1U << 3U>(i, j);
-        break;
-    case 2U:
-        trans_static<1U << 2U>(i, j);
-        break;
-    case 1U:
-        trans_static<1U << 1U>(i, j);
-        break;
-    case 0U:
-        break;
-    }
-}
-
-template<unsigned width>
-void trans_static(unsigned i, unsigned j) {
-    if constexpr (width > 1U) {
-        constexpr unsigned n_width = width / 2U;
-
-        trans_static<n_width>(i + n_width, j + n_width);
-
-        trans_static<n_width>(i + n_width, j);
-        swap_sq(i + n_width, j, n_width, i, j + n_width);
-        trans_static<n_width>(i + n_width, j);
-
-        trans_static<n_width>(i, j);
+        trans_swap(i, j, t_height, l_width); // top left
+        trans_swap(i + t_height, j, b_height, l_width); // bottom left
+        trans_swap(i + t_height, j + l_width, b_height, r_width); // bottom right
+        trans_swap(i, j + l_width, t_height, r_width); // top right
     }
 }
